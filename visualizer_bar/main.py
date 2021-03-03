02de9c2745d91f45e011a4582d087b1483abcdd9
ktarrant <kmtarrant13@gmail.com>
2021-03-03 02:05:35 -0500

    decent looking grid array

--- visualizer_bar/main.py
+++ visualizer_bar/main.py
@@ -1,22 +1,37 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
 from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
+from kivy.config import Config
 
 import numpy as np
 
-
-class GridArray(Widget):
-    pass
+Config.set('graphics', 'width', '850')
 
 
 class GridElement(Widget):
     pass
 
 
+class GridArray(GridLayout):
+    def __init__(self, **kwargs):
+        super(GridArray, self).__init__(**kwargs)
+        self.add_elements()
+
+    def add_elements(self):
+        for i in range(160):
+            element = GridElement()
+            self.add_widget(element)
+
+
+class GridController(Widget):
+    pass
+
+
 class GridApp(App):
     def build(self):
-        return GridArray()
+        return GridController()
 
 
 if __name__ == '__main__':
