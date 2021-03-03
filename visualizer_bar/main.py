4a18e628a8b28df521f8d13b5cc7532da3dcafc6
ktarrant <kmtarrant13@gmail.com>
2021-03-03 02:25:09 -0500

    add color property and bones of update methods

--- visualizer_bar/main.py
+++ visualizer_bar/main.py
@@ -1,28 +1,45 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ColorProperty
 from kivy.vector import Vector
 from kivy.config import Config
 
+from typing import List
 import numpy as np
 
 Config.set('graphics', 'width', '850')
 
 
+color_map = np.zeros(shape=(4, 40, 3), dtype=float)
+
+
 class GridElement(Widget):
-    pass
+    row = NumericProperty(0)
+    col = NumericProperty(0)
+    color = ColorProperty('#000000')
+
+    def update_color(self):
+        self.color = tuple(color_map[self.row, self.col])
 
 
 class GridArray(GridLayout):
     def __init__(self, **kwargs):
         super(GridArray, self).__init__(**kwargs)
-        self.add_elements()
+        self.elements = self.add_elements()
 
-    def add_elements(self):
-        for i in range(160):
-            element = GridElement()
-            self.add_widget(element)
+    def add_elements(self) -> List[GridElement]:
+        rv = []
+        for col in range(40):
+            for row in range(4):
+                element = GridElement(row=row, col=col)
+                self.add_widget(element)
+                rv.append(element)
+        return rv
+
+    def update_elements(self):
+        for element in self.elements:
+            element.update_color()
 
 
 class GridController(Widget):
